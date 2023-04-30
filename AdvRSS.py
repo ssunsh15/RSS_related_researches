4276571d0020996bd8e5764b9d16f15c7dfebe45
ssunsh15 <132155293+ssunsh15@users.noreply.github.com>
2023-04-30 13:15:16 +0900

    Advanced Materials RSS feeds

--- AdvRSS.py
+++ AdvRSS.py
@@ -1,2 +1,7 @@
+import pandas as pd
+import feedparser
+import requests
+from bs4 import BeautifulSoup
+
 parse_rss = feedparser.parse("https://onlinelibrary.wiley.com/feed/15214095/most-recent")
 print(parse_rss.entries[0].title)
