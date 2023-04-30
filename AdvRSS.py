1578befa5bfcabbfaa392e3f4194d2a859ff4e4c
ssunsh15 <132155293+ssunsh15@users.noreply.github.com>
2023-04-30 15:51:25 +0900

    Update AdvRSS.py

--- AdvRSS.py
+++ AdvRSS.py
@@ -6,7 +6,15 @@ from bs4 import BeautifulSoup
 parse_rss = feedparser.parse("https://onlinelibrary.wiley.com/feed/15214095/most-recent")
 print(parse_rss.entries[0].title)
 
-f = open("keywords.txt", 'w')
-line = f.readline()
-print(line)
+f = open("keywords.txt", 'r')
+lines = f.readlines()
+
+keywords = []
+
+for line in lines:
+    key = line.split('\n')
+    keywords.append(key[0]);
+    
 f.close()
+
+print(keywords)
