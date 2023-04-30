ade4eb3dbe2e2a1b01f335cae3391e28a7fd79d1
ssunsh15 <132155293+ssunsh15@users.noreply.github.com>
2023-04-30 21:07:03 +0900

    RSS from Advanced Materials
    RSS from the journal Advanced Materials
    - keyword filtering
    - generate HTML description from new RSS feeds

--- RSSAdvancedMaterials.py
+++ RSSAdvancedMaterials.py
@@ -1,13 +1,10 @@
-import pandas as pd
 import feedparser
-import requests
-from bs4 import BeautifulSoup
 
 # ----------------------------- RSS crawling and parsing from the journal "Advanced Materials" (sort : most recent)
 
 parse_rss = feedparser.parse("https://onlinelibrary.wiley.com/feed/15214095/most-recent")
 
-# RSS filtering keywords import from "DatabaseKeywords.txt"
+# ----------------------------- RSS filtering keywords import from "DatabaseKeywords.txt"
 
 f = open("DatabaseKeywords.txt", 'r')
 lines = f.readlines()
@@ -17,6 +14,19 @@ keywords = []
 for line in lines:
     key = line.split('\n')
     keywords.append(key[0])
+    
+f.close()
+
+# ----------------------------- Import Previous DOIs
+
+f = open("DatabaseAdvanDOI.txt", 'r')
+lines = f.readlines()
+
+Database = []
+
+for line in lines:
+    key = line.split('\n')
+    Database.append(key[0])
     
 f.close()
 
@@ -43,34 +53,57 @@ for p in parse_rss.entries:
                     break
                 else:
                     continue
-                    
-print(Sequence) #   -----------------------------------------------------------------------------
 
-# ----------------------------- RSS duplicated test from txt database
+# ----------------------------- Only new feed detection
 
-f = open("DatabaseAdvanDOI.txt", 'r')
-lines = f.readlines()
-
-Database = []
-
-for line in lines:
-    key = line.split('\n')
-    Database.append(key[0])
-    
-f.close()
-
-newDOI = []             # DOIs of new feeds
-newSequence = []        # parse_rss.entries[i], i for new feed & include keywords
+newDOI = []
+newSequence = []
 i = -1
 
 for new in RelatedRSSDOI:
     i = i + 1
+    valid = True
     for prev in Database:
         if new in prev:
+            valid = False
             break
         else:
             continue
-    newDOI.append(new)
-    newSequence.append(Sequence[i])
-   
-print(newSequence) #   -----------------------------------------------------------------------------
+    if valid:
+        newDOI.append(new)
+        newSequence.append(Sequence[i])
+    else:
+        continue
+        
+# ----------------------------- Database renewal
+
+f = open("DatabaseAdvanDOI.txt", 'w')
+
+Database = newDOI + Database
+
+for doi in Database:
+    f.write(doi + '\n')
+    
+f.close()
+
+# ----------------------------- new feed summary html generator
+
+j=0
+
+for i in newSequence:
+    html_text = ("<p>" + parse_rss.entries[i].title + "<p>" + 
+    parse_rss.entries[i].published + "<br>" + 
+    parse_rss.entries[i].link + "<br>" + 
+    parse_rss.entries[i].content[1].value)
+    
+    if j<10:
+        num = '00'+str(j)
+    elif j<100:
+        num = '0'+str(j)
+    else:
+        num = str(j)
+    
+    html_file = open('newFeeds/Advan' + num + '.html', 'w', encoding='utf-16')
+    html_file.write(html_text)
+    html_file.close()
+    j = j + 1
