45e3f7b8b4c8be7a69f5e13b655f8778143f127b
ssunsh15 <132155293+ssunsh15@users.noreply.github.com>
2023-04-30 16:45:46 +0900

    Update AdvRSS.py

--- AdvRSS.py
+++ AdvRSS.py
@@ -2,9 +2,14 @@ import pandas as pd
 import feedparser
 import requests
 from bs4 import BeautifulSoup
+import pickle
+
+# RSS crawling and parsing from the journal "Advanced Materials" (sort : most recent)
 
 parse_rss = feedparser.parse("https://onlinelibrary.wiley.com/feed/15214095/most-recent")
-print(parse_rss.entries[0].title)
+print(parse_rss.entries[0].title)       # parsing validation
+
+# RSS filtering keywords import from "keywords.txt"
 
 f = open("keywords.txt", 'r')
 lines = f.readlines()
@@ -13,8 +18,25 @@ keywords = []
 
 for line in lines:
     key = line.split('\n')
-    keywords.append(key[0]);
+    keywords.append(key[0])
     
 f.close()
 
-print(keywords)
+# RSS keyword filtering
+
+RelatedRSSDOI = []
+
+for p in parse_rss.entries:
+    if p == None:
+        break
+    else:
+        for keyword in keywords:
+            if keyword in p.title:
+                RelatedRSSDOI.append(p.id)
+                break
+            else:
+                continue
+
+print(RelatedRSSDOI)
+
+# RSS duplicated test from pickle database
